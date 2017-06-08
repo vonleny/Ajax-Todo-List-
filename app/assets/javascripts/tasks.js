4b33fd0fe75516fa72ca5be3dd93ac35775f4f5c
Misael Bejarano <vonleny@gmail.com>
2017-06-08 13:00:16 -0400

    First commit

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,11 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+$(document).on('turbolinks:load', function(){
+	console.log("The Doc is loading ", new Date());
+	$('#task_name').focus();
+	$('#save').click(function(evt){
+		evt.preventDefault();
+		console.log("Clicked");
+		// $('.edit_form').submit();
+	});
+});
